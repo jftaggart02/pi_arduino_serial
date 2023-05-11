88af49c0cabc6f01c729be470682ded92357ae9a
Joshua Taggart <jftaggart02@gmail.com>
2023-05-11 15:47:07 -0600

    got pi_control_rgb to work!

--- pi_control_rgb/pi_control_rgb.ino
+++ pi_control_rgb/pi_control_rgb.ino
@@ -1,82 +1,39 @@
-class RGBLED {
-  public:
-    int redPin;
-    int greenPin;
-    int bluePin;
-    RGBLED(int r, int g, int b);
-    void setColor(int red, int green, int blue);
-};
-
-
+#include "rgb_led.h"
 
 // Create RGB LED "LED1"
-// Red pin: 12
+// Red pin: 10
 // Green pin: 11
-// Blue Pin: 10
-RGBLED LED1(12, 11, 10);
+// Blue Pin: 12
+// ID: '1'
+RGBLED LED1(10, 11, 12);
 
 
 
-void setup() {}
+void setup()
+{
+
+  // Begin serial communicaion at 9600 baud
+  Serial.begin(9600);
+
+  // Send message to computer saying, "I'm ready!"
+  Serial.write('R');
+
+}
 
 
 
+byte data;
 int i;
+
+
 
 void loop() 
 {
 
-  // Transition from red through orange and yellow
-  for (i = 0; i <= 255; i++) {
-    LED1.setColor(255, i, 0);
-    delay(20);
+  // Read 3 bytes and set color of LED1 accordingly
+  if (Serial.available() >= 3)
+  {
+    LED1.setColor(Serial.read(), Serial.read(), Serial.read());
   }
 
-  // Transition to green
-  for (i = 255; i >= 0; i--) {
-    LED1.setColor(i, 255, 0);
-    delay(10);
-  }
-
-  // Transition to teal
-  for (i = 0; i <= 255; i++) {
-    LED1.setColor(0, 255, i);
-    delay(10);
-  }
-
-  // Transition to blue
-  for (i = 255; i >= 0; i--) {
-    LED1.setColor(0, i, 255);
-    delay(10);
-  }
-
-  // Transition to pink
-  for (i = 0; i <= 255; i++) {
-    LED1.setColor(i, 0, 255);
-    delay(10);
-  }
-  
-  // And back to red
-  for (i = 255; i >= 0; i--) {
-    LED1.setColor(255, 0, i);
-    delay(10);
-  }
-
-}
-
-
-
-void RGBLED::setColor(int red, int green, int blue) {
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);
-}
-
-RGBLED::RGBLED(int r, int g, int b) {
-  redPin = r;
-  greenPin = g;
-  bluePin = b;
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
 }
--- pi_control_rgb/pi_control_rgb.py
+++ pi_control_rgb/pi_control_rgb.py
@@ -2,41 +2,87 @@
 import serial
 from time import sleep
 
+
+# global variables
 delay = 0.01
-waiting = True
+arduino_ready_signal = 'R'
+
 
 # begin serial communication at baud of 9600
 # run ls dev/tty* and see if there's an "ACMX" or "USBX"
 # and change the serial address below accordingly.
-if __name__ == '__main__':
-    ser = serial.Serial('COM6', 9600)
-    ser.reset_input_buffer()
+ser = serial.Serial('COM6', 9600)
+
+
+# sends serial message to arduino which it uses to 
+# change the color of the RGB LED
+def setColor(r, g, b):
+    ser.write(r.to_bytes())
+    ser.write(g.to_bytes())
+    ser.write(b.to_bytes())
+    
+    
+# waits for the arduino to send a character indicating
+# that it's ready to receive data
+def wait_for_arduino():
+    global arduino_ready_signal
+    waiting = True
     
     print("Waiting...")
-    
-    while waiting == True:
+
+    while waiting:
         if ser.in_waiting > 0:
             print("Received something.")
             char = ser.read(1).decode('utf-8')
-            if char == '1':
+            if char == arduino_ready_signal:
                 print("Arduino is ready.")
                 waiting = False
                 
     print("Done waiting.")
-  
-    while True:
-        # slowly increase LED values
-        for x in range(0, 256, 2):
-            ser.write(b'1')
-            ser.write(x.to_bytes())
-            ser.write(b'2')
-            ser.write(x.to_bytes())
-            sleep(delay)
-        # slowoy decrease LED values
-        for x in range(255, 0, -2):
-            ser.write(b'1')
-            ser.write(x.to_bytes())
-            ser.write(b'2')
-            ser.write(x.to_bytes())
-            sleep(delay)
+    
 
+# tells the arduino to make the LED cycle through color spectrum
+def color_cycle():
+    # transition from red to yellow
+    for x in range(256):
+        setColor(255, x, 0)
+        sleep(delay)
+    
+    # transition to green
+    for x in range(255, 0, -1):
+        setColor(x, 255, 0)
+        sleep(delay)
+        
+    # transition to teal
+    for x in range(256):
+        setColor(0, 255, x)
+        sleep(delay)
+    
+    # transition to blue
+    for x in range(255, 0, -1):
+        setColor(0, x, 255)
+        sleep(delay)
+    
+    # transition to pink
+    for x in range(256):
+        setColor(x, 0, 255)
+        sleep(delay)
+    
+    # transition back to red
+    for x in range(255, 0, -1):
+        setColor(255, 0, x)
+        sleep(delay)
+        
+    
+# in case there was anything in the input buffer we didn't want,
+# we clear it
+ser.reset_input_buffer()
+
+
+# wait for arduino to be ready
+wait_for_arduino()
+
+
+while True:
+    color_cycle()
+    
