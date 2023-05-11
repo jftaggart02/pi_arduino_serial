f6eda21aba1424feda392f921b9377badb675386
Joshua Taggart <jftaggart02@gmail.com>
2023-05-11 12:40:26 -0600

    got pi_control_arduino to work!

--- pi_control_arduino/pi_control_arduino.ino
+++ pi_control_arduino/pi_control_arduino.ino
@@ -10,8 +10,10 @@ void setup()
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
 
-  // Start serial communication at 9600 baud
-  Serial.begin(9600);
+  // Start serial communication at 115200 baud
+  Serial.begin(115200);
+
+  Serial.write('1');
 
 }
 
@@ -21,31 +23,25 @@ int ledVal;
 void loop() 
 {
 
-  // Wait for reponse from pi
-  while (1)
+  if (Serial.available() > 1)
   {
-    if (Serial.available() > 1)
+    // Read 2 bytes
+    ledID = Serial.read();
+    ledVal = Serial.read();
+
+    // Write LEDs accordingly
+    if (ledID == '1')
     {
-      break;
+      analogWrite(LED1, ledVal);
     }
-  }
-
-  // Read 2 bytes
-  ledID = Serial.read();
-  ledVal = Serial.read();
-
-  // Write LEDs accordingly
-  if (ledID == 1)
-  {
-    analogWrite(LED1, ledVal);
-  }
-  else if (ledID == 2)
-  {
-    analogWrite(LED2, ledVal);
-  }
-  else
-  {
-    digitalWrite(ERRLED, HIGH);
+    else if (ledID == '2')
+    {
+      analogWrite(LED2, ledVal);
+    }
+    else
+    {
+      digitalWrite(ERRLED, HIGH);
+    }
   }
 
 }
