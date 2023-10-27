c6f0fd4d0d82b749e130930bf266e225f6b1e680
Joshua Taggart <jftaggart02@gmail.com>
2023-10-26 19:30:44 -0600

    started zomby_motor_control

---
+++ zomby_motor_control/main.py
@@ -0,0 +1,60 @@
+import serial
+from time import sleep
+
+ser = serial.Serial('COM6', 9600)
+
+class Motor:
+    def __init__(self, ID, ser):
+        # initialize motor ID
+        self.ID = ID
+        # pass reference to serial object
+        self.ser = ser
+
+    def setSpeed(self, speed):
+        # send motor ID to arduino
+        self.ser.write(self.ID)
+
+        # constrain motor speed to value from 0 to 100
+        if speed < 0:
+            speed = 0
+        if speed > 100:
+            speed = 100
+
+        # send motor speed to arduino
+        self.ser.write(speed.to_bytes(length=1, byteorder='big'))
+
+def wait_for_arduino():
+
+    # waits to receive this character
+    arduino_ready_signal = 'R'
+    
+    waiting = True
+    
+    print("Waiting...")
+
+    while waiting:
+        if ser.in_waiting > 0:
+            char = ser.read(1).decode('utf-8')
+            if char == arduino_ready_signal:
+                print("Arduino is ready.")
+                waiting = False
+                
+    print("Done waiting.")
+
+motor_right = Motor(b"r", ser)
+motor_left = Motor(b"l", ser)
+
+wait_for_arduino()
+
+while True:
+    for x in range(0, 100):
+        speed = x
+        motor_right.setSpeed(speed)
+        motor_left.setSpeed(speed)
+        sleep(0.020)
+    
+    for x in range(100, 0):
+        speed = x
+        motor_right.setSpeed(speed)
+        motor_left.setSpeed(speed)
+        sleep(0.020)
