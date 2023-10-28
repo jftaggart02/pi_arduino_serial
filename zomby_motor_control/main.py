2935fa7d77560060f74f56dbf422fd63c74fb7cd
Joshua Taggart <jftaggart02@gmail.com>
2023-10-28 16:48:15 -0600

    zomby_motor_control: minor adjustments and added docs

--- zomby_motor_control/main.py
+++ zomby_motor_control/main.py
@@ -4,21 +4,52 @@ from time import sleep
 
 
 class Motor:
-    # STATIC CLASS VARIABLES -------------------------------------------------
-    motor_slew_delay = 0.05 # units are in seconds
+    # NOTES -------------------------------------------------------------------
+        # Motor speed ranges from 0 (full reverse) to 64 (stopped) to 128 (full forward) 
+
+    # USAGE ------------------------------------------------------------------
+        # Example: 
+            # Initialize serial connection with arduino:
+            # ser = serial.Serial('COM6', 9600)
+
+            # Instantiate two Motor objects using char ID and serial object
+            # motor_right = Motor(b"r", ser)
+            # motor_left = Motor(b"l", ser)
+            
+            # Enable motor slew rate
+            # motor_right.setSlew(True)
+            # motor_left.setSlew(True)
+
+            # Run right motor full forwards and left motor full backwards for 5 seconds
+            # motor_right.setSpeed(128)
+            # motor_left.setSpeed(0)
+            # sleep(5)
+
+            # Stop motors
+            # motor_right.setSpeed(64)
+            # motor_left.setSpeed(64)
+
+
+    # STATIC CLASS VARIABLES --------------------------------------------------
+    motor_slew_time = 3 # time (seconds) it takes for motors to spin from stopped to full speed
+    motor_slew_delay = motor_slew_time / 64
     motor_count = 0
 
 
     # constructor
     def __init__(self, ser_ID, ser):
-        # PRIVATE MEMBER VARIABLES ------------------------------------------------
+        # PRIVATE MEMBER VARIABLES --------------------------------------------
         self.__ID = Motor.motor_count
         Motor.motor_count += 1
-        self.__ser_ID = ser_ID # initialize motor ID we send over serial
-        self.__ser = ser # pass reference to serial object
-        self.__speed = 64 # default speed is stopped (64)
-        self.__desired_speed = 64
-        self.__slew = False # slew is off by default
+
+        self.__ser_ID = ser_ID # sets motor ID char that gets sent over serial
+        self.__ser = ser # get reference to serial object
+
+        # default speed is stopped (64)
+        self.__speed = 64 # speed that gets sent to arduino every time self.__sendSpeed() is called
+        self.__desired_speed = 64 # desired speed that we increment self.__speed towards when motor slew is enabled
+
+        self.__slew = False # slew is disabled by default
         self.__stop_slew = threading.Event() # false by default, when set to true, it stops the slew thread
         
 
@@ -33,13 +64,15 @@ class Motor:
 
     def __slew_function(self):
         while self.__stop_slew.is_set() == False:
-            # slew functionality
+
             if self.__desired_speed < self.__speed:
                 self.__speed += 1
                 self.__sendSpeed()
+
             elif self.__desired_speed > self.__speed:
                 self.__speed += 1
                 self.__sendSpeed()
+
             sleep(Motor.motor_slew_delay)
 
 
@@ -69,19 +102,20 @@ class Motor:
             self.__stop_slew.set()
 
 
-# begin serial communication with arduino
-ser = serial.Serial('COM6', 9600)
+    def setSlewRate(self, slew_time):
+        Motor.motor_slew_time = slew_time
+        Motor.motor_slew_delay = Motor.motor_slew_time / 64
 
 
 # waits for arduino to indicate that it's ready
-def wait_for_arduino():
+def wait_for_arduino(ser):
 
     # waits to receive this character
     arduino_ready_signal = 'R'
     
     waiting = True
     
-    print("Waiting...")
+    print("Waiting for Arduino...")
 
     while waiting:
         if ser.in_waiting > 0:
@@ -90,24 +124,29 @@ def wait_for_arduino():
                 print("Arduino is ready.")
                 waiting = False
                 
-    print("Done waiting.")
+    print("Arduino is ready.")
 
 
-motor_right = Motor(b"r", ser)
-motor_left = Motor(b"l", ser)
-
-
-# -------------------------------------
+# -----------------------------------------------------------------------------
 # Here's the main part of the program:
-# -------------------------------------
+# -----------------------------------------------------------------------------
 if __name__ == "__main__":
 
+    # begin serial communication with arduino
+    ser = serial.Serial('COM6', 9600)
+
     # wait for arduino to be ready to receive data
-    wait_for_arduino()
+    wait_for_arduino(ser)
+
+    motor_right = Motor(b"r", ser)
+    motor_left = Motor(b"l", ser)
 
     motor_right.setSlew(True)
+    motor_left.setSlew(True)
 
     motor_right.setSpeed(128)
+    motor_left.setSpeed(0)
     sleep(5)
+    
     motor_right.setSpeed(64)
-    sleep(5)
+    motor_left.setSpeed(64)
